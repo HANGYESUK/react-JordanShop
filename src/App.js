85b176ab0ce07c59e51fe780ec2829369885422e
HANGYESUK <akakakak66@naver.com>
2022-02-15 20:10:16 +0900

    셋팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Container, Nav, Button }  from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Shop</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      <div className='jumboTron'>
+        <h1>20% Season of!</h1>
+        <p>어쩌구 저쩌구 아싸라비야</p>
+        <Button variant="primary">Primary</Button>
+      </div>
+
     </div>
   );
 }
